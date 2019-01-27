e0b30e2665a2525718472e1ff378f84a5311ac33
Drew S. Ortega <DrewSOrtega@gmail.com>
2019-01-26 16:56:17 -0800

    add compare and combine of recursive results

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import math
 
+
 def parse_coords(file_path):
     coords = []
 
@@ -13,8 +14,27 @@ def parse_coords(file_path):
 
     return(coords)
 
-def distance(p1,p2):
-        dist_x = p2[0]-p1[0]
-        dist_y = p2[1]-p1[1]
-        dist = math.sqrt(dist_x**2 + dist_y**2)
-        return(dist)
+
+def compare(p1, p2, min_result):
+    dist = distance(p1, p2)
+    if dist < min_result[0]:
+        min_result = (dist, [(p1, p2)])
+    if dist == min_result[0]:
+        min_result[0].append((p1, p2))
+    return min_result
+
+
+def combine_results(res1, res2):
+    if res1[0] == res2[0]:
+        for point in res2[1]:
+            res1[1].append(point)
+    elif res2[0] < res1[0]:
+        return res2
+    return res1
+
+
+def distance(p1, p2):
+    dist_x = p2[0]-p1[0]
+    dist_y = p2[1]-p1[1]
+    dist = math.sqrt(dist_x**2 + dist_y**2)
+    return(dist)

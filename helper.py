62499360192c14a7d6a979c878f5d8d266451182
Taylor Griffin <taylor.griffin.365@gmail.com>
2019-01-26 17:18:06 -0800

    function to sort results

--- helper.py
+++ helper.py
@@ -18,3 +18,19 @@ def distance(p1,p2):
         dist_y = p2[1]-p1[1]
         dist = math.sqrt(dist_x**2 + dist_y**2)
         return(dist)
+
+def swap_tuple(tup):
+        return((tup[1],tup[0]))
+
+def sort_results(results):
+        # organize the points so the smaller point is first
+        for result in results:
+                if (result[0][0] > result[1][0]):
+                        swap_tuple(result)
+                elif (result[0][0] == result[1][0]):
+                        # if same x, choose point with lower y coord
+                        if (result[0][1] > result[1][1]):
+                                swap_tuple(result)
+        # organize the list of tuples
+        results.sort(key=lambda tup: (tup[0],tup[1]))
+        return(results)

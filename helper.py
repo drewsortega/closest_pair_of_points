c6f5ddbcecb12e6eff21333cfabf2a559d5e2bd1
Drew S. Ortega <DrewSOrtega@gmail.com>
2019-01-26 22:26:19 -0800

    completed dirty, working version of naive divide & conquer

--- helper.py
+++ helper.py
@@ -14,41 +14,62 @@ def parse_coords(file_path):
 
     return(coords)
 
-def distance(p1,p2):
-        dist_x = p2[0]-p1[0]
-        dist_y = p2[1]-p1[1]
-        dist = math.sqrt(dist_x**2 + dist_y**2)
-        return(dist)
+
+def distance(p1, p2):
+    dist_x = p2[0]-p1[0]
+    dist_y = p2[1]-p1[1]
+    dist = math.sqrt(dist_x**2 + dist_y**2)
+    return(dist)
+
 
 def swap_tuple(tup):
-        return((tup[1],tup[0]))
+    return((tup[1], tup[0]))
+
+
+def map_indices(points1, points2):
+    current_index = 0
+    mapped_indices = []
+    for (index, point) in enumerate(points2):
+        if len(mapped_indices) == len(points1):
+            return mapped_indices
+        if point[1] >= points1[current_index][1]:
+            mapped_indices.append(index)
+            current_index += 1
+    while(len(mapped_indices) < len(points1)):
+        mapped_indices.append(len(points2)-1)
+    return mapped_indices
+
 
 def sort_results(results):
-        # organize the points so the smaller point is first
-        for result in results:
-                if (result[0][0] > result[1][0]):
-                        swap_tuple(result)
-                elif (result[0][0] == result[1][0]):
-                        # if same x, choose point with lower y coord
-                        if (result[0][1] > result[1][1]):
-                                swap_tuple(result)
-        # organize the list of tuples
-        results.sort(key=lambda tup: (tup[0],tup[1]))
-        return(results)
+    # organize the points so the smaller point is first
+    for result in results:
+        if (result[0][0] > result[1][0]):
+            swap_tuple(result)
+        elif (result[0][0] == result[1][0]):
+            # if same x, choose point with lower y coord
+            if (result[0][1] > result[1][1]):
+                swap_tuple(result)
+    # organize the list of tuples
+    results.sort(key=lambda tup: (tup[0], tup[1]))
+    return(results)
+
 
 def compare(p1, p2, min_result):
     dist = distance(p1, p2)
-    if dist < min_result[0]:
+    if min_result[0] == None or dist < min_result[0]:
         min_result = (dist, [(p1, p2)])
-    if dist == min_result[0]:
-        min_result[0].append((p1, p2))
+    elif dist == min_result[0]:
+        min_result[1].append((p1, p2))
+    # print(p1[0], p1[1], p2[0], p1[1], min_result[0])
     return min_result
 
 
 def combine_results(res1, res2):
+    if res2[0] == None:
+        return res1
     if res1[0] == res2[0]:
         for point in res2[1]:
             res1[1].append(point)
-    elif res2[0] < res1[0]:
+    elif res1[0] == None or res2[0] < res1[0]:
         return res2
     return res1
--- naive_dc.py
+++ naive_dc.py
@@ -15,27 +15,34 @@ def closest_points(points):
 
     # Median Index
     med_index = math.ceil(num_points/2)
-    print(med_index)
+
+    left_points = points[:med_index]
+    right_points = points[med_index:]
 
     # Recursive calls TODO: check to see this is proper range
-    left_result = closest_points(points[:med_index-1])
-    right_result = closest_points(points[med_index:])
+    left_result = closest_points(left_points)
+    right_result = closest_points(right_points)
 
     min_result = helper.combine_results(left_result, right_result)
 
-    sorted_y_points = sorted(points, key=lambda tup: tup[1])
+    sorted_left_points = sorted(left_points, key=lambda tup: tup[1])
+    sorted_right_points = sorted(right_points, key=lambda tup: tup[1])
+    mapped_indices = helper.map_indices(
+        sorted_left_points, sorted_right_points)
+    # TODO: find out why there are duplicate comparisons
+    # TODO: use pretty print
 
     # check for smaller distance
-    for point_index, src_point in enumerate(sorted_y_points):
+    for point_index, src_point in enumerate(sorted_left_points):
         # compare points with y > src
-        for dest_point in sorted_y_points[point_index+1:]:
+        for dest_point in sorted_right_points[mapped_indices[point_index]:]:
             if min_result[0] != None and abs(dest_point[1] - src_point[1]) > min_result[0]:
                 break
             min_result = helper.compare(src_point, dest_point, min_result)
 
         # compare points with y < src
-        for dest_point in reversed(sorted_y_points[:point_index-1]):
-            if abs(dest_point[1] - src_point[1]) > min_result[0]:
+        for dest_point in reversed(sorted_right_points[:mapped_indices[point_index]]):
+            if min_result[0] != None and abs(dest_point[1] - src_point[1]) > min_result[0]:
                 break
             min_result = helper.compare(src_point, dest_point, min_result)
 
@@ -46,6 +53,6 @@ points = helper.parse_coords(sys.argv[1])
 sorted_points = sorted(points, key=lambda tup: tup[0])
 
 final_result = closest_points(sorted_points)
-# print(final_result[0])
-# for point in final_result[1]:
-#     print(point[0][0], point[0][1], point[1][0], point[1][1])
+print(final_result[0])
+for point in final_result[1]:
+    print(point[0][0], point[0][1], point[1][0], point[1][1])

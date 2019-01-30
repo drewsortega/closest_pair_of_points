586026fcc4dcb866f15f7ce38edc9539913b8084
Taylor Griffin <taylor.griffin.365@gmail.com>
2019-01-30 15:46:11 -0800

    added timer

--- brute_force.py
+++ brute_force.py
@@ -1,12 +1,15 @@
 import sys
 import math
 import helper
+import timeit
 
 input_file = sys.argv[1]
 coordinates = helper.parse_coords(input_file)
 min_distance = None
 min_distance_points = []
 
+# start timer
+start = timeit.default_timer()
 for src_idx, source in enumerate(coordinates):
     for dest in coordinates[src_idx:]:
         if (dest != source):
@@ -24,5 +27,10 @@ for src_idx, source in enumerate(coordinates):
                     min_distance = distance
                     min_distance_points.clear()
                     min_distance_points.append((source,dest))
+# stop timer
+stop = timeit.default_timer()
 
 helper.pp_results(min_distance,min_distance_points,"output_files/output_brute_force.txt")
+
+if(len(sys.argv) >= 3 and sys.argv[2] == "-v"):
+    print('\n---\nTime: ', stop - start)

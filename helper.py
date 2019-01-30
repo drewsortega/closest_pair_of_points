421eca186a718881ee93a64216ba0f09f2e1c03d
Taylor Griffin <taylor.griffin.365@gmail.com>
2019-01-29 19:50:00 -0800

    program to create random sample data sets

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
-import math
 import os
+import math
+import random
 
 
 def parse_coords(file_path):
@@ -80,3 +81,27 @@ def combine_results(res1, res2):
     elif res1[0] == None or res2[0] < res1[0]:
         return res2
     return res1
+
+
+def generate_sample_set(size,file_path):
+    # generate samples
+    points = []
+    x_points = random.sample(range(1, 1000000), size)
+    y_points = random.sample(range(1, 1000000), size)
+    for x_point, y_point, in zip(x_points,y_points):
+        points.append((x_point,y_point))
+    # write them to a file
+    if not os.path.exists("randomly_generated_points"):
+        os.makedirs("randomly_generated_points")
+    output = open(file_path,"w")
+    for point in points:
+        output.write("%d %d\n" % (point[0], point[1]))
+    output.close()
+
+# run this to create 4 sample sets of sizes 100, 1000, 10000, and 100000
+def create_samples():
+    sample_sizes = [10**2,10**3,10**4,10**5]
+    sample_file_names = ["100_points.txt","1000_points.txt","10000_points.txt","100000_points.txt"]
+    for size,name in zip(sample_sizes,sample_file_names):
+        generate_sample_set(size,"randomly_generated_points/%s" % name)
+

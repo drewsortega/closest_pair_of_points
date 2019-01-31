748011aa7b4147b0744c8fccc3e332775dc5caec
Taylor Griffin <taylor.griffin.365@gmail.com>
2019-01-30 16:28:56 -0800

    func for enhanced

--- helper.py
+++ helper.py
@@ -82,6 +82,12 @@ def combine_results(res1, res2):
         return res2
     return res1
 
+def get_points_from_sorted_y(master_list, x_min, x_max):
+    return_list = []
+    for point in master_list:
+        if (point[0] >= x_min and point[0] <= x_max):
+            return_list.append(point)
+    return return_list
 
 def generate_sample_set(size,file_path):
     # generate samples
@@ -104,4 +110,69 @@ def create_samples():
     sample_file_names = ["100_points.txt","1000_points.txt","10000_points.txt","100000_points.txt"]
     for size,name in zip(sample_sizes,sample_file_names):
         generate_sample_set(size,"randomly_generated_points/%s" % name)
+
+# Python program for implementation of MergeSort 
+
+# Merges two subarrays of arr[]. 
+# First subarray is arr[l..m] 
+# Second subarray is arr[m+1..r] 
+def merge(arr, l, m, r): 
+	n1 = m - l + 1
+	n2 = r- m 
+
+	# create temp arrays 
+	L = [0] * (n1) 
+	R = [0] * (n2) 
+
+	# Copy data to temp arrays L[] and R[] 
+	for i in range(0 , n1): 
+		L[i] = arr[l + i] 
+
+	for j in range(0 , n2): 
+		R[j] = arr[m + 1 + j] 
+
+	# Merge the temp arrays back into arr[l..r] 
+	i = 0	 # Initial index of first subarray 
+	j = 0	 # Initial index of second subarray 
+	k = l	 # Initial index of merged subarray 
+
+	while i < n1 and j < n2 : 
+		if L[i] <= R[j]: 
+			arr[k] = L[i] 
+			i += 1
+		else: 
+			arr[k] = R[j] 
+			j += 1
+		k += 1
+
+	# Copy the remaining elements of L[], if there 
+	# are any 
+	while i < n1: 
+		arr[k] = L[i] 
+		i += 1
+		k += 1
+
+	# Copy the remaining elements of R[], if there 
+	# are any 
+	while j < n2: 
+		arr[k] = R[j] 
+		j += 1
+		k += 1
+
+# l is for left index and r is right index of the 
+# sub-array of arr to be sorted 
+def mergeSort(arr,l,r): 
+	if l < r: 
+
+		# Same as (l+r)/2, but avoids overflow for 
+		# large l and h 
+		m = (l+(r-1))/2
+
+		# Sort first and second halves 
+		mergeSort(arr, l, m) 
+		mergeSort(arr, m+1, r) 
+		merge(arr, l, m, r)
+
+# This code is contributed by Mohit Kumra 
+
 
